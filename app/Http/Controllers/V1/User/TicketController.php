d902f3f4bb7e9509e81144c43557e26637972ec0
wyx2685 <wyx2685@gmail.com>
2024-12-25 02:13:25 +0800

    更新 TicketController.php

--- app/Http/Controllers/V1/User/TicketController.php
+++ app/Http/Controllers/V1/User/TicketController.php
@@ -32,7 +32,7 @@ class TicketController extends Controller
             $ticket['message'] = TicketMessage::where('ticket_id', $ticket->id)->get();
             for ($i = 0; $i < count($ticket['message']); $i++) {
                 if ($ticket['message'][$i]['user_id'] !== $ticket->user_id) {
-                    $ticket['message'][$i]['is_me'] = fasle;
+                    $ticket['message'][$i]['is_me'] = false;
                 } else {
                     $ticket['message'][$i]['is_me'] = true;
                 }
